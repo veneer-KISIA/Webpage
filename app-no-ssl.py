e4cd10ea26b52796ea508e4cabe2e77637bec615
jang siwoo <88125431+siwooJang@users.noreply.github.com>
2023-08-30 19:15:48 +0900

    Update app-no-ssl.py
    업로드시 STT 자동 반환

--- app-no-ssl.py
+++ app-no-ssl.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, send_file, jsonify
 import os
-import log
-from flask_sslify import SSLify  # SSL 적용을 위한 모듈
+import json
+from pydub import AudioSegment
+import whisper_timestamped as whisper
+import audio
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = 'uploads'
+STT_FOLDER = 'stt'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['STT_FOLDER'] = STT_FOLDER
 
 @app.route('/')
 def index():
@@ -12,48 +19,31 @@ def index():
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'file' not in request.files:
-        message='서버에서 받은 파일이 없습니다.'
-        i(message)
-        return jsonify(message=message), 400
-    
+        return jsonify(message='No file part'), 400
+
     file = request.files['file']
     if file.filename == '':
-        message='파일 이름이 공백입니다'
-        i(message)
-        return jsonify(message=message), 400
-    
+        return jsonify(message='No selected file'), 400
+
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
         file.save(filename)
-        i(f"파일이 업로드되었습니다: {filename}")
-        return jsonify(message='서버가 파일을 받았습니다', fileName=file.filename), 200
+
+        # STT 변환 실행
+        stt_result = audio.transcribe(filename)
+        # 여기서 stt_result 변수에 STT 결과가 저장됩니다.
+
+        return jsonify(message='파일이 정상적으로 업로드 되고 STT 변환되었습니다. ', fileName=file.filename, STT=stt_result), 200
+
+def transcribe_audio(audio_filepath):
+    audio = whisper.load_audio(audio_filepath)
+    model = whisper.load_model("tiny", device="cpu")
+    return whisper.transcribe(model, audio)
 
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
     filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
     return send_file(filepath, as_attachment=True)
 
-def setup_ssl():
-    ssl_home = '/etc/letsencrypt/live/veneer.r-e.kr/'
-    global ssl_fullchain 
-    ssl_fullchain = ssl_home + 'fullchain.pem'
-    global ssl_privkey 
-    ssl_privkey = ssl_home + 'privkey.pem'
-
-    # SSL 적용
-    return SSLify(app)
-
 if __name__ == '__main__':
-    UPLOAD_FOLDER = 'uploads'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-    # SSL 적용
-    # setup_ssl()
-
-    # 로거
-    logger = log.get(__name__, "./logs/app.log")
-    log.set_level(__name__, "i")
-    i, d = logger.info, logger.debug
-
     app.run(debug=True, host='0.0.0.0', port=9999)
-    i("서버가 9999포트에서 시작되었습니다")
