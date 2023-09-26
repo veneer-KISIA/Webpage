dd40b9e2bb129c4eef02b0bc0a2cfb0bb1e8cc90
hohihanhyeon <hohihanhyeon@gmail.com>
2023-09-26 17:44:47 +0900

    버그수정

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,128 +1,161 @@
 window.addEventListener('DOMContentLoaded', event => {
+
   // Navbar shrink function
   var navbarShrink = function () {
-      const navbarCollapsible = document.body.querySelector('#mainNav');
-      if (!navbarCollapsible) {
-          return;
-      }
-      if (window.scrollY === 0) {
-          navbarCollapsible.classList.remove('navbar-shrink')
-      } else {
-          navbarCollapsible.classList.add('navbar-shrink')
-      }
+    const navbarCollapsible = document.body.querySelector('#mainNav');
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove('navbar-shrink')
+    } else {
+      navbarCollapsible.classList.add('navbar-shrink')
+    }
+
   };
+
   // Shrink the navbar 
   navbarShrink();
+
   // Shrink the navbar when page is scrolled
   document.addEventListener('scroll', navbarShrink);
+
   // Activate Bootstrap scrollspy on the main nav element
   const mainNav = document.body.querySelector('#mainNav');
   if (mainNav) {
-      new bootstrap.ScrollSpy(document.body, {
-          target: '#mainNav',
-          rootMargin: '0px 0px -40%',
-      });
+    new bootstrap.ScrollSpy(document.body, {
+      target: '#mainNav',
+      rootMargin: '0px 0px -40%',
+    });
   };
+
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector('.navbar-toggler');
   const responsiveNavItems = [].slice.call(
-      document.querySelectorAll('#navbarResponsive .nav-link')
+    document.querySelectorAll('#navbarResponsive .nav-link')
   );
   responsiveNavItems.map(function (responsiveNavItem) {
-      responsiveNavItem.addEventListener('click', () => {
-          if (window.getComputedStyle(navbarToggler).display !== 'none') {
-              navbarToggler.click();
-          }
-      });
+    responsiveNavItem.addEventListener('click', () => {
+      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+        navbarToggler.click();
+      }
+    });
   });
+
 });
-document.addEventListener('DOMContentLoaded', function () {
+
+// document 로드 후 실행
+function onLoad() {
   const browseButton = document.getElementById('browseButton');
   const audioFileInput = document.getElementById('audioFile');
-  // Function to handle file upload
-  async function handleFileUpload(selectedFile) {
-    if (!selectedFile) {
-      alert('파일을 선택하세요.');
-      return;
-    }
-    const formData = new FormData();
-    formData.append("audio", selectedFile);
-    try {
-      const response = await fetch("/upload", {
-        method: "POST",
-        body: formData,
-      });
-      if (response.ok) {
-        const data = await response.json();
-        const fileName = data.fileName;
-        const fileData = { fileName: fileName };
-        console.log(data)
-        // Send the POST request to '/mask'
-        fetch('/mask', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(fileData)
-        })
-          .then(response => {
-            if (!response.ok) {
-              throw new Error(`Fail while masking. ${response.status}`);
-            }
-            return response.json();
-          })
-          .then(data => {
-            const stt_text = data.stt_text;
-            document.getElementById("stt-text").innerText = stt_text;
-            const masked_text = data.ner_text;
-            console.log('masked_text:', masked_text);
-            document.getElementById("masked-text").innerText = masked_text;
-            const maskedFileName = data.fileName;
-            if (maskedFileName == null) { // NER server error
-              return;
-            }
-            const maskedFileData = { fileName: maskedFileName };
-            // Send the POST request to '/download/masked'
-            fetch('/download/masked', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(maskedFileData)
-            })
-              .then(response => response.blob())
-              .then(audioBlob => {
-                const audioUrl = URL.createObjectURL(audioBlob);
 
-                let audio = document.createElement("audio");
-                audio.controls = true;
-                audio.src = audioUrl;
-                document.getElementById("masked-audio").innerText = ""
-                document.getElementById("masked-audio").appendChild(audio);
-              })
-              .catch(error => {
-                console.error('Error:', error);
-                return;
-              });
-          })
-          .catch(error => {
-            console.error('Error:', error);
-            alert(error)
-          });
-      } else {
-        alert(`File upload fail. ${response.status}`);
-      }
-    } catch (error) {
-      console.error('Error while uploading file:', error);
-      alert('File upload fail.');
-    }
-  }
   // Event listeners
   browseButton.addEventListener('click', function () {
     audioFileInput.click();
   });
+
   audioFileInput.addEventListener('change', async function (event) {
     const selectedFile = event.target.files[0];
     await handleFileUpload(selectedFile);
   });
-});
+}
+
+document.addEventListener('DOMContentLoaded', onLoad);
+
+async function handleFileUpload(selectedFile) {
+  // 파일 업로드
+  const data = await uploadAudioFile(selectedFile);
+
+  // 오디오 마스킹
+  const response_data = await maskAudioFile(data.fileName);
+
+  // STT 결과 표시
+  showSTTResult(response_data.stt_text, response_data.ner_text);
+
+  // 마스킹된 오디오 파일 다운로드
+  const audioBlob = await downloadMaskedAudioFile(response_data.fileName);
+
+  // 오디오 플레이어 생성
+  createAudioPlayer(audioBlob);
+}
+
+async function uploadAudioFile(selectedFile) {
+  if (!selectedFile) {
+    alert('파일을 선택하세요.');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append("audio", selectedFile);
+  const response = await fetch("/upload", {
+    method: "POST",
+    body: formData,
+  });
+
+  if (!response.ok) {
+    throw new Error(`파일 업로드 실패 (/upload) ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+async function maskAudioFile(fileName) {
+  const fileData = { fileName: fileName };
+
+  // Send the POST request to '/mask'
+  const response = await fetch('/mask', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(fileData)
+  });
+
+  if (!response.ok) {
+    throw new Error(`마스킹 오류 (/mask) ${response.status}`);
+  }
+
+  return await response.json();
+}
+
+function showSTTResult(stt_text, ner_text) {
+  document.getElementById("stt-text").innerText = stt_text;
+
+  const masked_text = ner_text;
+  console.log('masked_text:', masked_text);
+  document.getElementById("masked-text").innerText = masked_text;
+}
+
+async function downloadMaskedAudioFile(fileName) {
+  if (fileName == null) {
+    throw new Error(`NER 서버 에러. ${response.status}`);
+  }
+  const maskedFileData = { fileName: fileName };
+
+  // Send the POST request to '/download/masked'
+  const response = await fetch('/download/masked', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(maskedFileData)
+  });
+
+  if (!response.ok) {
+    throw new Error(`마스킹된 오디오 다운로드 실패 (/download/masked) ${response.status}`);
+  }
+
+  return await response.blob();
+}
+
+function createAudioPlayer(audioBlob) {
+  const audioUrl = URL.createObjectURL(audioBlob);
+
+  let audio = document.createElement("audio");
+  audio.controls = true;
+  audio.src = audioUrl;
+  document.getElementById("masked-audio").innerText = ""
+  document.getElementById("masked-audio").appendChild(audio);
+}
+
+
