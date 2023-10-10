20496228aa5f0fcbb2d677f5fc82242e372fb058
siwooJang <j35857834@gmail.com>
2023-10-10 23:13:51 +0900

    minor fix

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,66 +1,63 @@
-window.addEventListener('DOMContentLoaded', event => {
-
+window.addEventListener("DOMContentLoaded", (event) => {
   // Navbar shrink function
   var navbarShrink = function () {
-    const navbarCollapsible = document.body.querySelector('#mainNav');
+    const navbarCollapsible = document.body.querySelector("#mainNav");
     if (!navbarCollapsible) {
       return;
     }
     if (window.scrollY === 0) {
-      navbarCollapsible.classList.remove('navbar-shrink')
+      navbarCollapsible.classList.remove("navbar-shrink");
     } else {
-      navbarCollapsible.classList.add('navbar-shrink')
+      navbarCollapsible.classList.add("navbar-shrink");
     }
-
   };
 
-  // Shrink the navbar 
+  // Shrink the navbar
   navbarShrink();
 
   // Shrink the navbar when page is scrolled
-  document.addEventListener('scroll', navbarShrink);
+  document.addEventListener("scroll", navbarShrink);
 
   // Activate Bootstrap scrollspy on the main nav element
-  const mainNav = document.body.querySelector('#mainNav');
+  const mainNav = document.body.querySelector("#mainNav");
   if (mainNav) {
     new bootstrap.ScrollSpy(document.body, {
-      target: '#mainNav',
-      rootMargin: '0px 0px -40%',
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
     });
-  };
+  }
 
   // Collapse responsive navbar when toggler is visible
-  const navbarToggler = document.body.querySelector('.navbar-toggler');
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
   const responsiveNavItems = [].slice.call(
-    document.querySelectorAll('#navbarResponsive .nav-link')
+    document.querySelectorAll("#navbarResponsive .nav-link")
   );
   responsiveNavItems.map(function (responsiveNavItem) {
-    responsiveNavItem.addEventListener('click', () => {
-      if (window.getComputedStyle(navbarToggler).display !== 'none') {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
         navbarToggler.click();
       }
     });
   });
-
 });
 
 // document 로드 후 실행
 function onLoad() {
-  const browseButton = document.getElementById('browseButton');
-  const audioFileInput = document.getElementById('audioFile');
+  const browseButton = document.getElementById("browseButton");
+  const audioFileInput = document.getElementById("audioFile");
 
   // Event listeners
-  browseButton.addEventListener('click', function () {
+  browseButton.addEventListener("click", function () {
     audioFileInput.click();
   });
 
-  audioFileInput.addEventListener('change', async function (event) {
+  audioFileInput.addEventListener("change", async function (event) {
     const selectedFile = event.target.files[0];
     await handleFileUpload(selectedFile);
   });
 }
 
-document.addEventListener('DOMContentLoaded', onLoad);
+document.addEventListener("DOMContentLoaded", onLoad);
 
 async function handleFileUpload(selectedFile) {
   // 파일 업로드
@@ -73,8 +70,8 @@ async function handleFileUpload(selectedFile) {
   showSTTResult(response_data.stt_text, response_data.ner_text);
 
   //API 결과 표시
-  const moderation_data = response_data.moderation
-  showAPIresult(moderation_data)
+  const moderation_data = response_data.moderation;
+  showAPIresult(moderation_data);
 
   // 마스킹된 오디오 파일 다운로드
   const audioBlob = await downloadMaskedAudioFile(response_data.fileName);
@@ -85,7 +82,7 @@ async function handleFileUpload(selectedFile) {
 
 async function uploadAudioFile(selectedFile) {
   if (!selectedFile) {
-    alert('파일을 선택하세요.');
+    alert("파일을 선택하세요.");
     return;
   }
 
@@ -107,12 +104,12 @@ async function maskAudioFile(fileName) {
   const fileData = { fileName: fileName };
 
   // Send the POST request to '/mask'
-  const response = await fetch('/mask', {
-    method: 'POST',
+  const response = await fetch("/mask", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(fileData)
+    body: JSON.stringify(fileData),
   });
 
   if (!response.ok) {
@@ -126,7 +123,7 @@ function showSTTResult(stt_text, ner_text) {
   document.getElementById("stt-text").innerText = stt_text;
 
   const masked_text = ner_text;
-  console.log('masked_text:', masked_text);
+  console.log("masked_text:", masked_text);
   document.getElementById("masked-text").innerText = masked_text;
 }
 
@@ -137,16 +134,18 @@ async function downloadMaskedAudioFile(fileName) {
   const maskedFileData = { fileName: fileName };
 
   // Send the POST request to '/download/masked'
-  const response = await fetch('/download/masked', {
-    method: 'POST',
+  const response = await fetch("/download/masked", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
-    body: JSON.stringify(maskedFileData)
+    body: JSON.stringify(maskedFileData),
   });
 
   if (!response.ok) {
-    throw new Error(`마스킹된 오디오 다운로드 실패 (/download/masked) ${response.status}`);
+    throw new Error(
+      `마스킹된 오디오 다운로드 실패 (/download/masked) ${response.status}`
+    );
   }
 
   return await response.blob();
@@ -158,30 +157,27 @@ function createAudioPlayer(audioBlob) {
   let audio = document.createElement("audio");
   audio.controls = true;
   audio.src = audioUrl;
-  document.getElementById("masked-audio").innerText = ""
+  document.getElementById("masked-audio").innerText = "";
   document.getElementById("masked-audio").appendChild(audio);
 }
 
 function showAPIresult(moderation_data) {
   const progressBarsContainer = document.getElementById("api-result");
 
-  moderation_data.forEach(item => {
-    
+  moderation_data.forEach((item) => {
     const textSpan = document.createElement("span");
-    textSpan.textContent = `${item.name}: ${item.confidence.toFixed(2)}%`;
+    textSpan.textContent = `${item.name}: ${item.confidence.toFixed(2) * 100}%`;
 
     const container = document.createElement("div");
     container.classList.add("progress-container");
-    
+
     const progressBar = document.createElement("progress");
     progressBar.value = item.confidence;
     progressBar.max = 1;
-    
-    container.appendChild(textSpan)
+
+    container.appendChild(textSpan);
     container.appendChild(progressBar);
 
     progressBarsContainer.appendChild(container);
   });
 }
-
-
